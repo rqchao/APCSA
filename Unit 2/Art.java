f2f625da7bf04927a8bdebbcc9da25445e8ed6de
rqchao <chaoqrichard@gmail.com>
2018-10-03 11:33:13 -0700

    looks better

--- Unit 2/Art.java
+++ Unit 2/Art.java
@@ -1,30 +1,35 @@
 public class Art {
-public static final String[] CHARS = new String[] { "u", "▂", "▃", "▄", "▅", "▆", "▇", "█" };
+public static final String[] CHARS = new String[] { "▁", "▂", "▃", "▄", "▅", "▆", "▇", "█" };
 public static final String[] COLORS = new String[] { "\u001b[31m", "\u001b[32m", "\u001b[33m", "\u001b[34m",
                                                      "\u001b[35m", "\u001b[36m", "\u001b[37m", "\u001b[0m" };
 
 public static void main(String[] args) {
         for (int line = 0; line < 10; line++) {
-                for (int pyrmamid = line; pyrmamid < line + 7; pyrmamid++) {
-                        color(pyrmamid % 7);
+                for (int pyramid = line; pyramid < line + 7; pyramid++) {
                         for (int block = 0; block < 8; block++) {
-                                printChar(block);
+                                printChar(block, block);
                         }
                         for (int block = 7; block >= 0; block--) {
-                                printChar(block);
+                                printChar(block, block);
+                        }
+                }
+                System.out.println();
+                for (int pyramid = line; pyramid < line + 7; pyramid++) {
+                  for (int block = 7; block >= 0; block--) {
+                          printChar(block, block);
+                  }
+                        for (int block = 0; block < 8; block++) {
+                                printChar(block, block);
                         }
                 }
                 System.out.println();
         }
 }
 
-public static void color(int n) {
-        System.out.printf(COLORS[n]);
 
-}
-
-public static void printChar(int n) {
-        System.out.printf(CHARS[n]);
+public static void printChar(int n, int m) {
+          System.out.printf(COLORS[n]);
+        System.out.printf(CHARS[m]);
 
 }
 }
